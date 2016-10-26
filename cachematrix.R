73a33b1597518ab8bfa3c7aa17c531290cd17c22
karolathebest <karolathebest@gmail.com>
2016-10-27 00:07:57 +0200

    Caching matrix with dedicated object

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Creates cacheable matrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversed <- NULL
+        set <- function(y) {
+                x <<- y
+                inversed <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(i) inversed <<- i
+        getInverse <- function() inversed
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x' 
+## using a cache if the 'x' does not change
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inversed <- x$getInverse()
+        if(!is.null(inversed)) {
+                message("getting cached data")
+                return(inversed)
+        }
+        
+        data <- x$get()
+        inversed <- solve(data, ...)
+        x$setInverse(inversed)
+        return (inversed)
 }
+
